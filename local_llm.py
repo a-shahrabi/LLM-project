8fc774d98d7e0f909f39a4dec766437603f3dea9
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-04-07 23:00:46 -0400

    Check gpu

--- local_llm.py
+++ local_llm.py
@@ -15,3 +15,11 @@ from torch.utils.data import DataLoader, Dataset
 from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup, BertForSequenceClassification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, f1_score, ConfusionMatrixDisplay, confusion_matrix
+
+# If there's a GPU available...
+if torch.cuda.is_available():
+    # tell PyTorch to use the GPU.
+    device = torch.device("cuda")
+
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('we will use the GPU:', torch.cuda.get_device_name(0))
